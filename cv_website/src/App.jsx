fc05050b7c4111db689e25fb25c4b2a80bd9e7d5
Theodore Dimopoulos <tdimopoulosprof@gmail.com>
2023-02-09 15:46:32 +0200

    first feb

--- cv_website/src/App.jsx
+++ cv_website/src/App.jsx
@@ -3,12 +3,16 @@ import React from 'react'
 //Component imports
 import Navigation from  './components/navigation/navigation'
 import Header from './components/header/header'
+import About from './components/about/about'
+import Experience from './components/experience/experience'
 
 const App = () => {
     return (
         <>
         <Navigation />
         <Header />
+        <About />
+        <Experience />
         </>
     )
 }
