238e5b5bdbc3d91c974e4b85840d57fa53a7cbad
Theodore Dimopoulos <tdimopoulosprof@gmail.com>
2023-02-21 17:18:05 +0200

    Update experience.jsx

--- cv_website/src/components/experience/experience.jsx
+++ cv_website/src/components/experience/experience.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
+import './experience.css'
+import {MdOutlineVerified} from 'react-icons/md'
 
 const experience = () => {
   return (
     <section id='experience'>
 
-        experience
+        <h5>What Skills I Have</h5>
+        <h2>My Experience</h2>
+
+        <div className='container experience__container'>
+          <div className='experience__frontend'>
+            <h3>Frontend Development</h3>
+            <div className='experience__content'>
+              <article className='experience__details'>
+                <MdOutlineVerified />
+                <h4>Lorem</h4>
+                <small className='text-light'>Lorem Ipsum</small>
+              </article>
+              <article className='experience__details'>
+                <MdOutlineVerified />
+                <h4>Lorem</h4>
+                <small className='text-light'>Lorem Ipsum</small>
+              </article>
+              <article className='experience__details'>
+                <MdOutlineVerified />
+                <h4>Lorem</h4>
+                <small className='text-light'>Lorem Ipsum</small>
+              </article>
+              <article className='experience__details'>
+                <MdOutlineVerified />
+                <h4>Lorem</h4>
+                <small className='text-light'>Lorem Ipsum</small>
+              </article>
+              <article className='experience__details'>
+                <MdOutlineVerified />
+                <h4>Lorem</h4>
+                <small className='text-light'>Lorem Ipsum</small>
+              </article>
+            </div>
+          </div>
+        </div>
 
     </section>
   )
