081a4c4e5006b3acbbeeafe1dba1a5521f1dc72d
Theodore Dimopoulos <tdimopoulosprof@gmail.com>
2023-03-01 16:52:03 +0200

    job experience component

--- cv_website/src/components/experience/experience.jsx
+++ cv_website/src/components/experience/experience.jsx
@@ -1,10 +1,110 @@
 import React from 'react'
+import {GrFormCheckmark} from 'react-icons/gr'
 
 const experience = () => {
   return (
     <section id='experience'>
 
-        experience
+        <h5>What has brought me here</h5>
+        <h2>Job Experience</h2>
+
+        <div className='container experience__container'>
+
+
+            <article className='experience'>
+
+                <div className='experience__head'>
+                    <h3>Lorem Ipsum</h3>
+                </div>
+
+                <ul className='experience__list'>
+                    <li>
+                        <GrFormCheckmark className='experience__list-icon'/>
+                        <p>Lorem asdasdasdas djdsf n;dasjnf;sdnfkjanfkjnfk;jwnaefndkjnfkajsfd</p>
+                    </li>
+                    <li>
+                        <GrFormCheckmark className='experience__list-icon'/>
+                        <p>Lorem asdasdasdas djdsf n;dasjnf;sdnfkjanfkjnfk;jwnaefndkjnfkajsfd</p>
+                    </li>
+                    <li>
+                        <GrFormCheckmark className='experience__list-icon'/>
+                        <p>Lorem asdasdasdas djdsf n;dasjnf;sdnfkjanfkjnfk;jwnaefndkjnfkajsfd</p>
+                    </li>
+                    <li>
+                        <GrFormCheckmark className='experience__list-icon'/>
+                        <p>Lorem asdasdasdas djdsf n;dasjnf;sdnfkjanfkjnfk;jwnaefndkjnfkajsfd</p>
+                    </li>
+                    <li>
+                        <GrFormCheckmark className='experience__list-icon'/>
+                        <p>Lorem asdasdasdas djdsf n;dasjnf;sdnfkjanfkjnfk;jwnaefndkjnfkajsfd</p>
+                    </li>
+                </ul>
+            </article>
+
+
+            <article className='experience'>
+
+                <div className='experience__head'>
+                    <h3>Lorem Ipsum</h3>
+                </div>
+
+                <ul className='experience__list'>
+                    <li>
+                        <GrFormCheckmark className='experience__list-icon'/>
+                        <p>Lorem asdasdasdas djdsf n;dasjnf;sdnfkjanfkjnfk;jwnaefndkjnfkajsfd</p>
+                    </li>
+                    <li>
+                        <GrFormCheckmark className='experience__list-icon'/>
+                        <p>Lorem asdasdasdas djdsf n;dasjnf;sdnfkjanfkjnfk;jwnaefndkjnfkajsfd</p>
+                    </li>
+                    <li>
+                        <GrFormCheckmark className='experience__list-icon'/>
+                        <p>Lorem asdasdasdas djdsf n;dasjnf;sdnfkjanfkjnfk;jwnaefndkjnfkajsfd</p>
+                    </li>
+                    <li>
+                        <GrFormCheckmark className='experience__list-icon'/>
+                        <p>Lorem asdasdasdas djdsf n;dasjnf;sdnfkjanfkjnfk;jwnaefndkjnfkajsfd</p>
+                    </li>
+                    <li>
+                        <GrFormCheckmark className='experience__list-icon'/>
+                        <p>Lorem asdasdasdas djdsf n;dasjnf;sdnfkjanfkjnfk;jwnaefndkjnfkajsfd</p>
+                    </li>
+                </ul>
+            </article>
+
+            
+            <article className='experience'>
+
+                <div className='experience__head'>
+                    <h3>Lorem Ipsum</h3>
+                </div>
+
+                <ul className='experience__list'>
+                    <li>
+                        <GrFormCheckmark className='experience__list-icon'/>
+                        <p>Lorem asdasdasdas djdsf n;dasjnf;sdnfkjanfkjnfk;jwnaefndkjnfkajsfd</p>
+                    </li>
+                    <li>
+                        <GrFormCheckmark className='experience__list-icon'/>
+                        <p>Lorem asdasdasdas djdsf n;dasjnf;sdnfkjanfkjnfk;jwnaefndkjnfkajsfd</p>
+                    </li>
+                    <li>
+                        <GrFormCheckmark className='experience__list-icon'/>
+                        <p>Lorem asdasdasdas djdsf n;dasjnf;sdnfkjanfkjnfk;jwnaefndkjnfkajsfd</p>
+                    </li>
+                    <li>
+                        <GrFormCheckmark className='experience__list-icon'/>
+                        <p>Lorem asdasdasdas djdsf n;dasjnf;sdnfkjanfkjnfk;jwnaefndkjnfkajsfd</p>
+                    </li>
+                    <li>
+                        <GrFormCheckmark className='experience__list-icon'/>
+                        <p>Lorem asdasdasdas djdsf n;dasjnf;sdnfkjanfkjnfk;jwnaefndkjnfkajsfd</p>
+                    </li>
+                </ul>
+            </article>
+
+            
+        </div>
         
     </section>
   )
